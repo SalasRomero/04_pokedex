18193711b24f9f5fa1cd9db784ffd790df0d266d
David Salas <dasa.19983@gmail.com>
2024-09-20 22:49:23 -0500

    app de pokedex

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: '04_pokedex',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true
+    }
+  }
 };
 
 export default config;
---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  private nextUrl:string;
+
+  constructor() { 
+    this.nextUrl = 'https://pokeapi.co/api/v2/pokemon?offset=00&limit=20';
+  }
+
+  getPokemons(){
+    const url = this.nextUrl;
+
+    if(url){
+
+    }
+    return null;
+  }
+}
